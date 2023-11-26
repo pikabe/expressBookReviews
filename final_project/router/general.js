4ecef4ab7dfce9170f71f9b09bd700da07291c44
pikabe <35191200+pikabe@users.noreply.github.com>
2023-11-26 16:23:55 +0000

    Get book details based on author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -35,8 +35,15 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const getBooks = new Promise((resolve, reject) => {
+        const author = req.params.author;
+        const bookList = Object.values(books).filter(book => book.author === author);
+        if (bookList.length > 0) {
+          resolve(res.status(200).json(bookList));
+        } else {
+          reject(res.status(404).json({ message: `The books are not found by author: ${author}` }));
+        }
+    });
 });
 
 // Get all books based on title
