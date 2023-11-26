841e33867779c40946344f72536ec3156b51cf55
pikabe <35191200+pikabe@users.noreply.github.com>
2023-11-26 19:24:02 +0000

    delete review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -45,6 +45,19 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   }
 });
 
+// regd_users.delete("/auth/review/:isbn", (req, res) => {
+//     const isbn = req.params.isbn;
+//     const username = req.session.authorization.username;
+//     let book = books[isbn];
+//     if (book) {
+//         delete book.reviews[username];
+//         return res.status(200).send(`Review successfully deleted for ${isbn}, by ${username}`);
+//     }
+//     else {
+//         return res.status(404).json({message: `ISBN ${isbn} not found`});
+//     }
+// });
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
