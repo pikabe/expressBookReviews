e7f7c9b18a2ed10db5b9955204afb1d922294f8b
pikabe <35191200+pikabe@users.noreply.github.com>
2023-11-26 18:23:59 +0000

    login registered users

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -12,14 +12,23 @@ const isValid = (username)=>{ //returns boolean
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+  return users.some(user => user.username === username && user.password === password);
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Write your code here
+    const username = req.body.username;
+    const password = req.body.password;
+    if (authenticatedUser(username, password)) {
+        let accessToken = jwt.sign({data:password}, "access", {expiresIn: 3600});
+        req.session.authorization = {accessToken,username};
+        return res.status(200).send("Customer successfully logged in");
+    }
+    else {
+        return res.status(208).json({message: "Invalid username or password"});
+    }
 });
-
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
